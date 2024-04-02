cbd2b2b9b9ae9335f779855063241584635fabf3
Heet Doshi <doshiheet2872003@gmail.com>
2024-04-02 13:09:03 +0530

    API Done

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { PostService } from '../services/post.service';
 
 @Component({
   selector: 'app-post',
@@ -12,12 +12,20 @@ import { PostService } from '../services/post.service';
 })
 export class PostComponent implements OnInit {
 
-  private postService = inject(PostService)
+  private http = inject(HttpClient)
   posts : any =[]
 
   // ngoninit is lifecycle hook.
   ngOnInit(): void {
       this.loadPosts()
+
+  }
+  loadPosts(){
+    this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe(
+      (res:any)=>{
+        this.posts = res
+      }
+    )
   }
   // fetchPosts(){
   //   this.postService.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
@@ -31,16 +39,4 @@ export class PostComponent implements OnInit {
   //     this.posts = posts
   //   });
   // }
-
-  loadPosts(){
-    this.postService.getPosts().subscribe({
-      next:(posts  :any) =>{
-        this.posts = posts;
-        console.log('Post fetched',posts)
-      },
-      error:(error)=>{
-        console.log('Error fetching posts',error)
-      }
-    })
-  }
 }
